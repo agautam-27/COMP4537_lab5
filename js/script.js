de3ecfa29d8860fecee3e1ccfd53878c1ac6ade2
agautam-27 <122254669+agautam-27@users.noreply.github.com>
2025-02-15 21:58:29 -0800

    Fixed insertData function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,13 @@ function insertData() {
     fetch(backendUrl, { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ query: "INSERT INTO patient (name, dateOfBirth) VALUES ('John Doe', '1985-02-15')" }) 
+        body: JSON.stringify({ 
+            query: `INSERT INTO patient (name, dateOfBirth) VALUES 
+            ('Sara Brown', '1901-01-01'),
+            ('John Smith', '1941-01-01'),
+            ('Jack Ma', '1961-01-30'),
+            ('Elon Musk', '1999-01-01')`
+        })        
     })        
     .then(response => response.text())
     .then(data => {
